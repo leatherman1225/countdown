bf4cd2f3785bf7100fe1793eb60a7962f36e2554
Brock Leatherman <bleatherman@java.nas.local>
2013-10-28 10:27:23 -0400

    Added java file

---
+++ Countdown.java
@@ -0,0 +1,12 @@
+class Countdown {
+	public static void main (String[] args) {
+	  countdown(99);
+}
+	public static void countdown(int n) {
+	 	int x=n-1;
+		if(n > 99) {
+	 	}else{
+		countdown(n-1);
+ }
+  }
+   }
